0826939594e4c4a28f6bb2378e7b2287de3be7f5
xuezhiyou <644351021@qq.com>
2017-03-30 11:23:58 +0800

    new add

---
+++ 1.sh
@@ -0,0 +1,10 @@
+./configure \
+    --disable-strip \
+    --disable-werror \
+    --target-list=i386-softmmu \
+    --with-sdlabi="2.0" \
+    --disable-xen \
+    --enable-o3-opt \
+    --enable-tb-lookup-opt \
+    #--enable-tcg-interpreter
+    #--test--
